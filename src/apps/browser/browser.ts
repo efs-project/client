744693e2a0dc72dcc3ff563deb1bb259b1d30f8b
James Carnley <jamescarnley@gmail.com>
2025-02-19 05:08:23 -0600

    Recursive now

--- src/apps/browser/browser.ts
+++ src/apps/browser/browser.ts
@@ -29,22 +29,54 @@ export class EfsBrowser extends SignalWatcher(LitElement) {
 
     // TODO: Move me somewhere else
     // const uid = "0x6e4851b1ee4ee826a06a4514895640816b4143bf2408c33e5c1263275daf53ce";
-    const uid = "0x21792c13ed1e2b20c3827ec68c644dd8a79b9c137c6e2f21aa9ccc9cafc1b7a5"; // Bryce's attestation
-    const schema = "0xb96446c85ce538c1641a967f23ea11bbb4a390ef745fc5a9905689dbd48bac86"; // Schema of Dahk's attestation
-    getAttestation(uid);
-    const isIndexed = await efs.isAttestationIndexed(uid);
-    const refCount = await efs.getReferencingAttestationUIDCount(uid, schema);
-    const refUIDs = await efs.getReferencingAttestationUIDs(uid, schema);
-    console.log('isIndexed', isIndexed);
-    console.log('refCount', refCount);
-    console.log('refUIDs', refUIDs);
 
-    // foreach ref in refUIDs call getAttestation(ref)
-    refUIDs.forEach(async (ref) => {
-      getAttestation(ref);
-    });
+    // const uid = "0x21792c13ed1e2b20c3827ec68c644dd8a79b9c137c6e2f21aa9ccc9cafc1b7a5"; // Bryce's attestation
+    // const schema = "0xb96446c85ce538c1641a967f23ea11bbb4a390ef745fc5a9905689dbd48bac86"; // Schema of Dahk's attestation
+    // getAttestation(uid);
+    // const isIndexed = await efs.isAttestationIndexed(uid);
+    // const refCount = await efs.getReferencingAttestationUIDCount(uid, schema);
+    // const refUIDs = await efs.getReferencingAttestationUIDs(uid, schema);
+    // console.log('isIndexed', isIndexed);
+    // console.log('refCount', refCount);
+    // console.log('refUIDs', refUIDs);
+
+    // // foreach ref in refUIDs call getAttestation(ref)
+    // refUIDs.forEach(async (ref) => {
+    //   getAttestation(ref);
+    // });
+
+        const uid = "0x6e4851b1ee4ee826a06a4514895640816b4143bf2408c33e5c1263275daf53ce";
+        const schema = "0xddc07ff085923cb9a3c58bf684344b7672881e5a004044e3e99527861fed6435";
+
+
+
+        await this.#traverseAttestations(uid, schema);
+    }
+    
+    async #traverseAttestations(uid: string, schema: string, depth: number = 0) {
+        console.log('Depth:', depth, 'Processing attestation:', uid);
+        
+        // Get the attestation data
+        await getAttestation(uid);
+        
+        // Check if it's indexed and get references
+        const isIndexed = await efs.isAttestationIndexed(uid);
+        if (!isIndexed) {
+            console.log('Attestation not indexed:', uid);
+            return;
+        }
+    
+        // Get and process references
+        const refUIDs = await efs.getReferencingAttestationUIDs(uid, schema);
+        console.log('Found', refUIDs.length, 'references at depth', depth);
+    
+        // Recursively process each reference
+        for (const ref of refUIDs) {
+            await this.#traverseAttestations(ref, schema, depth + 1);
+        }
+    }
   }
-}
+
 
   declare global {
     interface HTMLElementTagNameMap {
